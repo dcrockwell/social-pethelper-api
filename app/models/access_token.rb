56aaef28bcfd3bc5f47cd2b8e3a22f678879b698
D.C. Rockwell <dcrockwell@DCs-MacBook-Pro.local>
2020-08-28 19:22:56 -0600

    feat: AccessToken secure random - securely generates a new random token

--- app/models/access_token.rb
+++ app/models/access_token.rb
@@ -1,6 +1,16 @@
+require 'securerandom'
+
 class AccessToken < ApplicationRecord
     scope :expired, -> { where('expires_at <= ?', DateTime.now)}
     scope :active, -> { where('expires_at > ?', DateTime.now)}
 
     belongs_to :user
+    
+    before_create :generate_token
+
+    private
+
+    def generate_token
+        self.token = SecureRandom.hex
+    end
 end
--- spec/models/access_token_spec.rb
+++ spec/models/access_token_spec.rb
@@ -1,10 +1,11 @@
 require 'rails_helper'
+require 'securerandom'
 
 describe AccessToken, type: :model do
   let(:user) { User.new }
   let(:token) { 'abc123' }
-  let(:expired_token) { AccessToken.create!(user: user, token: token, expires_at: Date.today - 1) }
-  let(:active_token) { AccessToken.create!(user: user, token: token, expires_at: Date.today + 1) }
+  let(:expired_token) { AccessToken.create!(user: user, expires_at: Date.today - 1) }
+  let(:active_token) { AccessToken.create!(user: user, expires_at: Date.tomorrow) }
 
   it 'scopes expired tokens' do
     expect(AccessToken.expired).to include(expired_token)
@@ -16,5 +17,10 @@ describe AccessToken, type: :model do
 
   it 'belongs to user' do
     expect(AccessToken.reflect_on_association(:user).macro).to eq(:belongs_to)
+  end
+
+  it 'generates a random digest for the token' do
+    expect(SecureRandom).to receive(:hex).and_return token
+    expect(active_token.token).to eql(token)
   end
 end
