dd2a6010173662775801cd7af298aa1c0c9bea99
D.C. Rockwell <dcrockwell@DCs-MacBook-Pro.local>
2020-08-28 19:31:28 -0600

    refactor: Application Controller Spec - cleaned up with rubocop - minor formatting changes

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -1,56 +1,54 @@
 require 'rails_helper'
 
-describe ApplicationController, :type => :controller do
-    let(:valid_token) { 'abc123' }
-    let(:user_name) { 'Bob T. Builder' }
-    let(:user) { User.create!(name: user_name, password: 'abc123', password_confirmation: 'abc123' ) }
+describe ApplicationController, type: :controller do
+  let(:user_name) { 'Bob T. Builder' }
+  let(:user) { User.create!(name: user_name, password: 'abc123', password_confirmation: 'abc123') }
+  let(:access_token) { AccessToken.create!(user: user, expires_at: Date.tomorrow) }
 
+  controller do
+    skip_before_action :authenticate!, only: [:index]
+
+    def index
+      raise ApplicationController::NotAuthorizedError
+    end
+
+    def create
+      render json: { user: current_user.name }, status: 200
+    end
+  end
+
+  describe 'authenticating' do
     before :each do
-        AccessToken.create!(user: user, token: valid_token, expires_at: Date.tomorrow)
+      request.headers['Authorization'] = access_token.token
+      get :create
     end
 
-    controller do
-        skip_before_action :authenticate!, only: [:index]
-
-        def index
-            raise ApplicationController::NotAuthorizedError
-        end
-
-        def create
-            render json: { user: current_user.name }, status: 200
-        end
+    it 'returns OK' do
+      expect(response).to have_http_status(:success)
     end
 
-    describe 'authenticating' do
-        before :each do
-            request.headers['Authorization'] = 'abc123'
-            get :create
-        end
+    it 'sets the current_user' do
+      expected_json = {
+        user: user_name
+      }
+      expect(response.body).to include_json(expected_json)
+    end
+  end
 
-        it 'returns OK' do
-            expect(response).to have_http_status(:success)
-        end
-
-        it 'sets the current_user' do
-            expect(response.body).to include_json({
-                "user": user_name
-            })
-        end
+  describe 'handling Unauthorized exceptions' do
+    before :each do
+      get :index
     end
 
-    describe 'handling Unauthorized exceptions' do
-        before :each do
-            get :index
-        end
-
-        it 'renders an unauthorized error' do
-            expect(response.body).to include_json({
-                "error": "Unauthorized"
-            })
-        end
-
-        it 'returns an unauthorized status code' do
-            expect(response).to have_http_status(:unauthorized)
-        end
+    it 'renders an unauthorized error' do
+      expected_json = {
+        error: 'Unauthorized'
+      }
+      expect(response.body).to include_json(expected_json)
     end
+
+    it 'returns an unauthorized status code' do
+      expect(response).to have_http_status(:unauthorized)
+    end
+  end
 end
